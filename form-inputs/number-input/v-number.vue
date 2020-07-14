dacf6a291779f585a0404db211e26ca54b26dc21
Saleh Mirnezami <algo@hey.com>
2020-07-14 13:15:56 +0430

    add v-number components

---
+++ form-inputs/number-input/v-number.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="inline-flex">
+    <div class="select-none border py-2 px-4 cursor-pointer bg-gray-100 hover:bg-gray-200 rounded-l"
+         @click="decrease">
+      -
+    </div>
+
+    <input class="border p-2 text-center outline-none" type="text" v-model="number" :name="name">
+
+    <div class="select-none border py-2 px-4 cursor-pointer bg-gray-100 hover:bg-gray-200 rounded-r"
+         @click="increase">
+      +
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        number: 1
+      }
+    },
+    props: {
+      name: {
+        type: String,
+        required: true
+      }
+    },
+    methods: {
+      increase() {
+        this.number++;
+      },
+      decrease() {
+        if (this.number > 1) {
+          this.number--;
+        }
+      },
+    }
+  };
+</script>
