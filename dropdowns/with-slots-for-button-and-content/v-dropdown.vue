834b64c9ff40c4295aff9b814a7dc5a4a27348ee
Saleh Mirnezami <algo@hey.com>
2020-07-22 12:45:02 +0430

    add transition to v-dropdown's content

--- dropdowns/with-slots-for-button-and-content/v-dropdown.vue
+++ dropdowns/with-slots-for-button-and-content/v-dropdown.vue
@@ -8,11 +8,20 @@
     <button class="fixed inset-0 h-full w-full cursor-default focus:outline-none" v-if="open" @click="open = false" tabindex="-1"></button>
 
     <!--dropdown menu-->
-    <div class="absolute shadow-lg border w-48 rounded py-1 px-2 text-sm mt-4 bg-white z-10"
-         :class="placement === 'right' ? 'right-0' : 'left-0'"
-         v-if="open">
-      <slot name="content"></slot>
-    </div>
+    <transition
+      enter-active-class="transition-all duration-200 ease-out"
+      leave-active-class="transition-all duration-750 ease-in"
+      enter-class="opacity-0 scale-75"
+      enter-to-class="opacity-100 scale-100"
+      leave-class="opacity-100 scale-100"
+      leave-to-class="opacity-0 scale-75"
+    >
+      <div class="absolute shadow-lg border w-48 rounded py-1 px-2 text-sm mt-4 bg-white z-10"
+           :class="placement === 'right' ? 'right-0' : 'left-0'"
+           v-if="open">
+        <slot name="content"></slot>
+      </div>
+    </transition>
   </div>
 </template>
 
